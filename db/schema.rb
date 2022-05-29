3e2129d0b9d570063ec2ef035cdd06124773f31b
Felipe Cicolin Rocha <felipecicolinrocha@gmail.com>
2022-05-29 14:15:51 -0300

    Commit

--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_29_171042) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_admins_on_confirmation_token", unique: true
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
