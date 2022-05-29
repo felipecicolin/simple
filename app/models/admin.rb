fb18e4552fa372e3138c71ea8f97eefd5dd7cbf4
Felipe Cicolin Rocha <felipecicolinrocha@gmail.com>
2022-05-29 13:59:58 -0300

    Commit

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,7 +2,7 @@
 
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :confirmable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
